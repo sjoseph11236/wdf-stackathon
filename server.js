0a27e85e12c503f942a03adc50e574dd453058b1
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-05 18:48:06 -0500

    feat: set-up server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('api/videos', (req, res) => {
   res.send('hello');
 });
 
-const PORT = 5000;
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT}`);
