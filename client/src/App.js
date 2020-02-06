0d965cccaea0b0b1c4f9a59f54a5c40d7b935412
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-05 19:24:37 -0500

    connected server

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { 
+      customers: []
+    }
+  }
+  componentDidMount() {
+    fetch('/api/customers')
+      .then(res => res.json())
+      .then(customers => this.setState({customers}));
+  }
+
   render() {
+    const { customers } = this.state;
+    console.log("TCL: App -> render -> customers ", customers )
+    
     return (
       <div className="App">
         <h1>WDF STACKATHON</h1>
+        <ul>
+          {customers.map(customer => {
+            return <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
+          })}
+        </ul>
       </div>
     );
   }
