563381070adc6790310201c4d6d3036325af9e91
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-05 20:29:15 -0500

    chore: setup error handlers

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+require('dotenv').config();
+
 app.use(morgan('tiny'));
 app.use(express.json());
 
@@ -14,6 +16,25 @@ app.get('/api/customers', (req, res) => {
   ];
   res.json(customers);
 });
+
+
+function notFound(req, res, next) {
+  const error = new Error('Not Found');
+  res.status(404);
+  next(error);
+}
+
+
+function errorHandler(error, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: error.message
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
 
 const PORT = 8080;
 
